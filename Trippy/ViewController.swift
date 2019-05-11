59b8f25b10f1d8c7f4bd8bea8a696351f2894bc8
jaebooker <jaesonbooker@gmail.com>
2019-05-11 16:38:42 -0700

    first blood

---
+++ Trippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trippy
+//
+//  Created by Jaeson Booker on 5/11/19.
+//  Copyright © 2019 Jaeson Booker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
